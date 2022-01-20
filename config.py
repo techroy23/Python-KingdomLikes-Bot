5f3ab52321a323babb50beb233cef5541fb66b76
Techroy23 <53241974+techroy23@users.noreply.github.com>
2022-01-21 07:30:35 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+"""
+
+make sure that the username and password is inside the ""
+e.g.
+
+username = "user@email.xxx"
+password = "p@ssW0rD"
+
+"""
+
+username = ""
+password = ""
+headless = "true"
